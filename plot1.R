6efc99ca82628473fcb3692c01d43637957bbae7
PeterAltmann <30799110+PeterAltmann@users.noreply.github.com>
2017-09-11 00:28:11 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,14 +1,22 @@
+#checks for required packages
 if(!require(tidyverse)){
         install.packages("tidyverse")
         library(tidyverse)
 }
 
+if(!require(lubridate)){
+        install.packages("lubridate")
+        library(lubridate)
+}
+
+#reads and prepares file
 read.csv("household_power_consumption.txt", sep = ";", na.strings = c("?")) %>%
         tbl_df() %>%
         mutate(., Date = dmy(as.character(.$Date))) %>%
         filter(Date >= "2007-02-01" & Date <= "2007-02-02") %>%
         mutate(Time = hms(.$Time)) -> raw_data
 
+#produces png output
 png(filename = 'plot1.png', width = 480, height = 480, bg = "transparent")
 with(raw_data, hist(Global_active_power, 
                     col = "red", 

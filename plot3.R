829dd402190cfb6289b35675fb88ae3ec53b031d
PeterAltmann <30799110+PeterAltmann@users.noreply.github.com>
2017-09-11 00:29:49 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,15 @@
+#checks for required packages
+if(!require(tidyverse)){
+        install.packages("tidyverse")
+        library(tidyverse)
+}
+
+if(!require(lubridate)){
+        install.packages("lubridate")
+        library(lubridate)
+}
+
+#Reads file and prepares it
 read.csv("household_power_consumption.txt", sep = ";", na.strings = c("?")) %>%
         tbl_df() %>%
         mutate(., Date = dmy(as.character(.$Date))) %>%
@@ -5,6 +17,7 @@ read.csv("household_power_consumption.txt", sep = ";", na.strings = c("?")) %>%
         mutate(datetime = paste(Date, Time)) %>%
         mutate(datetime = ymd_hms(.$datetime)) -> raw_data
 
+#produces png output
 png(filename = 'plot3.png', width = 480, height = 480, bg = "transparent")
 
         with(raw_data, plot(datetime, Sub_metering_1,

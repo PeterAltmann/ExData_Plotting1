c86e6adfd9bab00872c7784ae220bc2d88be6447
PeterAltmann <30799110+PeterAltmann@users.noreply.github.com>
2017-09-11 00:29:05 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,15 @@
+#checks for required packages
+if(!require(tidyverse)){
+        install.packages("tidyverse")
+        library(tidyverse)
+}
+
+if(!require(lubridate)){
+        install.packages("lubridate")
+        library(lubridate)
+}
+
+#Reads file and prepares it
 read.csv("household_power_consumption.txt", sep = ";", na.strings = c("?")) %>%
         tbl_df() %>%
         mutate(., Date = dmy(as.character(.$Date))) %>%
@@ -5,6 +17,7 @@ read.csv("household_power_consumption.txt", sep = ";", na.strings = c("?")) %>%
         mutate(Date_Time = paste(Date, Time)) %>%
         mutate(Date_Time = ymd_hms(.$Date_Time)) -> raw_data
 
+#produces png output
 png(filename = 'plot2.png', width = 480, height = 480, bg = "transparent")
 with(raw_data, plot(Date_Time, Global_active_power, 
                     type = "l",
